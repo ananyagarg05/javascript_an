942b49e589075d13c6e5ff896fa1d26622e7b9f6
Ananya Garg ♡︎ <128985175+ananyagarg05@users.noreply.github.com>
2024-09-03 19:10:53 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all Js code as newer js/version
+
+//alert("hello") //error aa rha hai ispr,we are using
+//node js not browser thats why error
+//readability imp hai
+console.log(3 + 3);
+console.log("ananya");
+
+//window.alert() instructs the browser to display
+// a dialog with an optional message, and to wait
+// until the user dismisses the dialog.
+
+let name = "ananya ";
+let age = 18;
+let isLoggedIn = false;
+
+//data types
+//number => 2 to power 53 range
+//string both single and double qoutes are okay
+//boolean true /false
+//null -> standalone value,emptpy value hai null (0)
+//undefined -> variable declare kia hai sirf,define ni kia
+//eg let state; of undefined
+//symbol (react m use hoga),uniqueness find krne ke liye
+
+//object
+
+console.log(typeof "ananya"); //op is string
+console.log(typeof null);//op is object
+console.log(typeof undefined);//op is undefined
+
+
+
