9aedbb368bdbd187bd26388c336a14ef6e5c7b71
Ananya Garg ♡︎ <128985175+ananyagarg05@users.noreply.github.com>
2024-09-20 19:41:51 +0000

    arrays2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,31 +1,50 @@
 //node 02_basics/02_arrays.js
 
-const marvel_heros = ["thor", "Ironman", "spiderman"];
-const dc_heros = ["superman", "flash", "batman"];
+// const marvel_heros = ["thor", "Ironman", "spiderman"];
+// const dc_heros = ["superman", "flash", "batman"];
 
-marvel_heros.push(dc_heros);
+//add karo push use krke
+//marvel_heros.push(dc_heros);
 
-console.log(marvel_heros);
-console.log(marvel_heros[3][1]);
+//console.log(marvel_heros);
+//ab yaha pr array in array hogya tha
+//[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+//isliye thor 0, ironman 1  ,spiderman 2 and the whole next thing is 3
+//therefore [3] mein full aata
+//and [3][1] ,that is [3] ka index 1 pr flash hai
+//[3][0] pr super man hota
+//not a good syntax though
+//console.log(marvel_heros[3][1]); //flash
 
-const allHeros = marvel_heros.concat(dc_heros);
-console.log(allHeros);
+//push ki jagah concatenate kr skte the
+//better
+//op
+//array in array nhi hoga yahan
+//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
 
-// const all_new_heros = [...marvel_heros, ...dc_heros]
+//spread
+//kaanch ka glass
+//drop krdo spread hojayega
+//......spread ho rhi hai values
+//... lgao and concatenate spread hojaeygi
+//zyda code and add krne ke liye
+//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// const all_new_heros = [...marvel_heros, ...dc_heros];
+//console.log(all_new_heros);
 
-// // console.log(all_new_heros);
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 
-// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
 
-// const real_another_array = another_array.flat(Infinity)
-// console.log(real_another_array);
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({ name: "hitesh" })); // interesting
 
-// console.log(Array.isArray("Hitesh"))
-// console.log(Array.from("Hitesh"))
-// console.log(Array.from({name: "hitesh"})) // interesting
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
 
-// let score1 = 100
-// let score2 = 200
-// let score3 = 300
-
-// console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));
