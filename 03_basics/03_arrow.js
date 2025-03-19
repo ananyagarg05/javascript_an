ea1a4e5aaec7da5a5b52878101000679204940f8
Ananya Garg ♡︎ <128985175+ananyagarg05@users.noreply.github.com>
2025-03-19 11:08:01 +0000

    new

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,56 @@
+node 03_basics/03_arrows.js
+const user = {
+    username: "Ananya",
+    price: 999,
+
+    welcomeMessage: function() {
+        //this keyword is used to access the object properties
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
