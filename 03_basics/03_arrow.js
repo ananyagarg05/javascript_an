6dca31e69967dcda78834ac7a5367f3dac1213db
Ananya Garg ♡︎ <128985175+ananyagarg05@users.noreply.github.com>
2025-03-19 15:32:14 +0000

    iife

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,56 +1,59 @@
-node 03_basics/03_arrows.js
-const user = {
-    username: "Ananya",
-    price: 999,
+// //node 03_basics/03_arrow.js
 
-    welcomeMessage: function() {
-        //this keyword is used to access the object properties
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
-    }
+//arrow functions
+// ()=> {}
+// //browser have wndow object
+// //node have global object
+// const user = {
+//   username: "Ananya",
+//   price: 999,
 
-}
+//   welcomeMessage: function () {
+//     //this keyword is used to access the object properties
+//     console.log(`${this.username} , welcome to website`);
+//     console.log(this);
+//   },
+// };
 
-user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+// user.welcomeMessage();
+// user.username = "sam";
+// user.welcomeMessage();
 
 // console.log(this);
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
+// function chai() {
+//   let username = "Ananya";
+//   console.log(this.username);
 // }
 
-// chai()
+// // chai();
+// // const chai = function () {
+// //   let username = "Ananya";
+// //   console.log(this.username);
+// // };
 
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+// //arrow function
+// const chai = () => {
+//   let username = "Ananya";
+//   console.log(this);
+// };
 
-const chai =  () => {
-    let username = "hitesh"
-    console.log(this);
-}
+// // chai()
 
+// //agar { } use kiya hai to return keyword use karna padega , this is explicit return 
+// //agar ( ) use kiya hai to return keyword use  nahi karna padega
+// // const addTwo = (num1, num2) => {
+// //     return num1 + num2
+// // }
 
-// chai()
+// // const addTwo = (num1, num2) =>  num1 + num2
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+// // const addTwo = (num1, num2) => ( num1 + num2 )
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// // const addTwo = (num1, num2) => ({ username: "Ananya" });
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// // console.log(addTwo(3, 4));
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// // const myArray = [2, 5, 3, 7, 8]
 
-
-console.log(addTwo(3, 4))
-
-
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
+// // myArray.forEach()
